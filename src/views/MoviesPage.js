3f1d03a8e35f085af368067cba3e6b3c6740c4d2
Alexey Zyukov <aalex64@ukr.net>
2021-12-30 22:39:22 +0200

    updte page Movies

--- src/views/MoviesPage.js
+++ src/views/MoviesPage.js
@@ -1,5 +1,20 @@
 import PageHeading from '../components/PageHeading/PageHeading';
+import styles from './view.module.css';
 
 export default function MoviesPage() {
-  return <PageHeading text="Movies" />;
+  return (
+    <>
+      <PageHeading text="Movies" />
+      <label>
+        <input
+          type="text"
+          className={styles.inputField}
+          placeHolder="input movie to search"
+        ></input>
+        <button type="submit" className={styles.button}>
+          Search
+        </button>
+      </label>
+    </>
+  );
 }
